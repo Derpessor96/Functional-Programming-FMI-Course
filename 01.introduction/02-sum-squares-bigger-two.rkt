72c8ab3316c34eb91ce055bab221c7593918d5b4
Derpessor96 <ivan.tonchev96@gmail.com>
2017-10-19 19:02:31 +0300

    solved some of the first-week exercises

---
+++ 01.introduction/02-sum-squares-bigger-two.rkt
@@ -0,0 +1,36 @@
+#lang racket
+(require "helpers.rkt")
+(require rackunit)
+(require rackunit/text-ui)
+;1.2 - Съчинете процедура, която по дадени три числа, намира сумата от квадратите на по-големите две от тях.
+; За по-удобно, може да разбиете задачата на по-малки такива.
+
+(define (min a b)
+  (if (< a b)
+    a
+    b
+  )
+)
+
+(define (square a) (* a a))
+
+(define 
+  (squares-sum-bigger-two a b c)
+  (define min-number (min a (min b c)))
+  (- (+ (square a) (square b) (square c)) (square min-number))
+)
+
+
+(define tests
+  (test-suite
+    "Sum of squares tests"
+
+    (let ((a (random 10))
+          (b (random 10))
+          (c (random 10)))
+      
+    (check-true (all-equal? (map (lambda (args) (apply squares-sum-bigger-two args))
+                                (permute (list a b c)))))
+)))
+
+(run-tests tests 'verbose)
---
+++ 01.introduction/04-count-digits.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require rackunit)
+(require rackunit/text-ui)
+; 1.4 - Търсим процедура, която намира броя цифри на дадено число
+; Трябва да работи и за отрицателни числа.
+
+(define (count-digits number)
+  (cond 
+    ((< number 0) (count-digits (- number)))
+    ((< number 10) 1)
+    (else (+ 1 (count-digits (/ number 10))))
+  )
+)
+
+
+(define tests
+  (test-suite
+    "Count digits tests"
+
+    (test-case "Should count correctly"
+      (check-equal? (count-digits 1024) 4)
+    )
+    (test-case "Should work alright with negative numbers"
+      (check-equal? (count-digits -987421245) 9)
+    )
+    (test-case "Should work alright with digits"
+      (check-equal? (count-digits 9) 1)
+      (check-equal? (count-digits 0) 1)
+    )
+  )
+)
+
+
+(run-tests tests 'verbose)
