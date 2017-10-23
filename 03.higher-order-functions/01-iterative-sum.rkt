3d9f10bab2003a59e5f7f8e1978e655685c7326a
Derpessor96 <ivan.tonchev96@gmail.com>
2017-10-23 14:29:21 +0300

    Solved an exercise

---
+++ 03.higher-order-functions/01-iterative-sum.rkt
@@ -0,0 +1,27 @@
+#lang racket
+(require rackunit)
+(require rackunit/text-ui)
+
+; Функцията sum, която видяхме на упражнение.
+; Да стане по итеративен начин.
+
+(define (sum start end term next)
+  (define (sum-iterative start end term next accumulator)
+    (if (> start end)
+        accumulator
+        (sum-iterative (next start) end term next (+ (term start) accumulator))))
+
+  (sum-iterative start end term next 0))
+
+(define (id x) x)
+(define (inc x) (+ x 1))
+
+(define tests
+  (test-suite "Iterative sum tests"
+
+    (check-equal? (sum 1 100 id inc) 5050)
+    (check-equal? (sum 9 9 id inc) 9)
+  )
+)
+
+(run-tests tests 'verbose)
