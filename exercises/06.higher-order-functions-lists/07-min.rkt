0b376c2b05dc75056ddb0c4c0f1dd95e9b9646d3
Derpessor96 <ivan.tonchev96@gmail.com>
2017-11-13 18:41:43 +0200

    Solved a week 6 exercise

---
+++ exercises/06.higher-order-functions-lists/07-min.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require rackunit)
+(require rackunit/text-ui)
+(require "03-fold-left.rkt")
+
+; Искаме да намерим най-малкото число в списък, използвайки fold-left или fold-right
+(define (minimum xs)
+  (fold-left (lambda (x y) (if (< x y) x y)) (car xs) xs))
+
+(define tests
+ (test-suite "Min tests"
+   (letrec (; create-random-list прави списък от случайни числа с дължина len
+            (create-random-list (lambda (len)
+             (if (= len 0)
+                 '()
+                 ; random прави случайно число до аргумента си
+                 (cons (random 213939219) (create-random-list (- len 1))))))
+            ; Създаваме си случаен списък с 235 елемента
+            (random-list (create-random-list 235))
+            ; Взимаме най-малкия елемент от него
+            (minimum-of-random-list (minimum random-list))
+            ; Това го знаем
+            (any? (lambda (p? xs)
+              (cond ((null? xs) #f)
+                    ((p? (car xs)) #t)
+                    (else (any? p? (cdr xs)))))))
+     ; Казваме, че не съществува елемент от random-list, който е по-малък от минимума, който сме намерили
+     (check-true (not (any? (lambda (x) (< x minimum-of-random-list)) random-list))))
+ )
+) 
+
+(display "ACTUAL TESTS-----------------------------------------------------------\n")
+
+(run-tests tests 'verbose)
