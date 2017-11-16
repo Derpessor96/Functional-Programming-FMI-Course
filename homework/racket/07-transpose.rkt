c460c3ac0d374995df94092791be6e29fe38d466
Derpessor96 <ivan.tonchev96@gmail.com>
2017-11-16 16:15:21 +0200

    Added newly fixed tests to a homework task

--- homework/racket/07-transpose.rkt
+++ homework/racket/07-transpose.rkt
@@ -12,8 +12,8 @@
 (define tests
   (test-suite "Transpose tests"
     ; The first two tests are wrong!
-    (check-equal? (transpose (make-matrix (range 1 7) 2 3)) (make-matrix (range 1 7) 3 2))
-    (check-equal? (transpose (make-matrix (range 7 13) 3 2)) (make-matrix (range 7 13) 2 3))
+    (check-equal? (transpose (make-matrix (range 1 7) 2 3)) (make-matrix '(1 4 2 5 3 6) 3 2))
+    (check-equal? (transpose (make-matrix (range 7 13) 3 2)) (make-matrix '(7 9 11 8 10 12) 2 3))
     (check-equal? (transpose (make-matrix (range 1 5) 2 2)) (make-matrix '(1 3 2 4) 2 2))
   )
 )
